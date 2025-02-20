5f51018eedad21e61895c6afa4c009a65eec7cf2
Achilleas <achilleasthomas200706@gmail.com>
2025-02-19 19:01:41 -0600

    Update Split JSON files into smaller ones.r

--- Scripts/Split JSON files into smaller ones.r
+++ Scripts/Split JSON files into smaller ones.r
@@ -5,21 +5,18 @@
 library(rjson)
 library(glue)
 
-scaffolds <- list("Blanchard", "McConnell") # These names, the file paths and other parameters can change for different applications of this script.
+scaffolds <- list("Blanchard", "McConnell") # The names, the file paths and other parameters can change for different applications of this script.
 
 for (name in scaffolds) {
-    
     all_jobs <- fromJSON(file = glue("./All sequences {name} JSON.json"))
-    
+
     for (i in 0:((length(all_jobs) %/% 100) - 1)) {
         data <- c(all_jobs)[(1 + i * 100):(i * 100 + 100)]
         jsondata <- toJSON(data)
         write(jsondata, glue("./JSON split files/{name} {i+1}.json"))
-
     }
-    
+
     data <- c(all_jobs)[(1 + (length(all_jobs) %/% 100) * 100):length(all_jobs)]
     jsondata <- toJSON(data)
     write(jsondata, glue("./JSON split/{name} {length(all_jobs) %/% 100 + 1}.json"))
-    
 }

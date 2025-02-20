908cef76547d960f934acfa41f2e1d74e20cdc22
Achilleas <achilleasthomas200706@gmail.com>
2025-02-20 12:23:30 -0600

    Update Sample Linux commands.sh

--- Sample Linux commands.sh
+++ Sample Linux commands.sh
@@ -1,1 +1,4 @@
+# Achilleas Thomas (Achi-lleas) 02/20/2025:
+# This is a sample Linux script which can be used to replicate the sequence of steps used to perform our project, running each of the scripts one at a time.
+
 $

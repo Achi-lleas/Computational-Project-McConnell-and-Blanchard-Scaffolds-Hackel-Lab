d3cc7938d1fb0f7f3450b314a76485e2fe78e949
Achilleas <achilleasthomas200706@gmail.com>
2025-02-21 11:37:42 -0600

    Update Sample Linux commands.sh

--- Sample Linux commands.sh
+++ Sample Linux commands.sh
@@ -1,4 +1,25 @@
 # Achilleas Thomas (Achi-lleas) 02/20/2025:
 # This is a sample Linux script which can be used to replicate the sequence of steps used to perform our project, running each of the scripts one at a time.
+# Make sure to have every file in the correct place and with the correct format. Use additional commands if needed.
 
-$
+$ mkdir "./Blanchard-McConnell files"
+$ cd "./Blanchard-McConnell files"
+# Have the following files in the new directory correctly formatted: Parental sequences.txt, Initial paratope designs.txt, Mutations.txt and all the scripts
+$ python3 "Get McConnell sequences given parental sequence, initial paratope design and mutations.py"
+# Have the following file in the directory correctly formatted: Target IDs.txt
+$ python3 "Access sequences of targets through UniProt.py"
+# Have the following files in the directory correctly formatted: McConnell sequences.txt, Blanchard sequences.txt, Target sequences.txt
+$ python3 "Generate JSON for McConnell sequences.py"
+$ python3 "Generate JSON for Blanchard sequences.py"
+# Have the following files in the directory: Blanchard JSON.json, McConnell JSON.json
+$ Rscript "Split JSON file.r"
+# Run the jobs on AlphaFold3, and download the models
+$ unzip ~Downloads/*.zip
+$ mkdir "./Scaffold models"
+$ mv -v ~/Downloads/* "~/Blanchard-McConnell files/Scaffold models"
+$ mkdir "./Best seeds"
+$ mkdir "./cif files"
+$ python3 "Loop through AlphaFold3 models and compare scores.py"
+$ python3 "Move cif files for each scaffold into new directory.py"
+# Do the analysis on ChimeraX and have the following files in the directory correctly formatted: Amino acids participating in binding.txt
+$ python3 "Find amino acid enrichment.py"
